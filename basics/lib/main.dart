32d9a358a8de1f5e163943200abf1f1851964e7a
Barend Koorzen <koorzenb@gmail.com>
2022-08-22 09:29:09 +0200

    Mapping Lists to Widgets

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
 
-  void _answerQuestions() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -29,11 +29,11 @@ class _MyAppState extends State<MyApp> {
     var questions = [
       {
         "questionText": 'What\'s your favorite color?',
-        "answers": ['Black', 'Red', 'White'],
+        "answers": ['Black', 'Red', 'White']
       },
       {
         "questionText": 'What\'s your favorite animal?',
-        "answers": ['Bear', 'puma', 'dolphin'],
+        "answers": ['Bear', 'puma', 'dolphin']
       }
     ];
 
@@ -44,10 +44,11 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestions),
-            Answer(_answerQuestions),
-            Answer(_answerQuestions),
+            Question(questions[_questionIndex]["questionText"]),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
       ),
