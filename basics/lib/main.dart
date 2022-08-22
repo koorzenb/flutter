e84cc7d5d7b6e083685b38555916f126744a2978
Barend Koorzen <koorzenb@gmail.com>
2022-08-22 13:34:34 +0200

    Calculating total score

--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -20,23 +20,33 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       "questionText": 'What\'s your favorite color?',
-      "answers": ['Black', 'Red', 'White']
+      "answers": [
+        {'text': 'Black', 'score': 1},
+        {'text': 'Red', 'score': 2},
+        {'text': 'White', 'score': 3}
+      ]
     },
     {
       "questionText": 'What\'s your favorite animal?',
-      "answers": ['Bear', 'puma', 'dolphin']
+      "answers": [
+        {'text': 'Bear', 'score': 1},
+        {'text': 'puma', 'score': 2},
+        {'text': 'dolphin', 'score': 3}
+      ]
     }
   ];
 
   int _questionIndex = 0;
+  int score = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int newScore) {
     if (_questionIndex < _questions.length) {}
 
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    score += newScore;
   }
 
   @override
@@ -52,7 +62,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 answerQuestion: _answerQuestion,
               )
-            : Result(),
+            : Result(score),
       ),
     );
   }
--- basics/lib/result.dart
+++ basics/lib/result.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int score;
+
+  Result(this.score);
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You didit!"),
+      child: Text('You didit! Your score is ${score}'),
     );
   }
 }
