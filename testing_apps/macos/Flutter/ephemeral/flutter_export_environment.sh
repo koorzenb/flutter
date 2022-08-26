68b18e287055b0d07746ff5249f0c86839cd8e6d
Barend Koorzen <koorzenb@gmail.com>
2022-08-26 13:42:33 +0200

    Initial fetch code

--- testing_apps/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ testing_apps/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\development\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\development\src\flutter\testing_apps"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\koorz\Documents\Workspace\flutter\testing_apps"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
